c573eccc37d3ad9d04063f5d0e4401cd20f7f0a8
Dinhbach02 <trandinhbach02@hmail.com>
2024-01-24 23:11:33 +0700

    CSS Text Color

--- NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f137bfc8fa461147ff4b06c7a99af843694a77b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6c468722bf75717f855c59d71fb39e6738742f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
