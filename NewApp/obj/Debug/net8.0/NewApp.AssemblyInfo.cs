c8c7cb07736d0cdc5d79d5423f9c4b7b2fe28a1b
Dinhbach02 <trandinhbach02@hmail.com>
2024-01-25 14:34:33 +0700

    CSS Layout - The display Property

--- NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6c468722bf75717f855c59d71fb39e6738742f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3deef8c5a7bd1f541a874452c2a9d66b563797f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
